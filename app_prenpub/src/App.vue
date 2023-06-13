cee7781fecf7e664cd3f2a782207b75c0266b8ee
Sergio-21-ops <sergio.diaz@davinci.edu.ar>
2023-06-13 08:29:28 -0300

    cambio msg

--- app_prenpub/src/App.vue
+++ app_prenpub/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Estamos probando vue 2 por cliS"/>
   </div>
 </template>
 
